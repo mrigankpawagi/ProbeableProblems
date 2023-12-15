84a003b56cad81ba32f2c2e4fb8877b2f9a31afb
Mrigank Pawagi <mrigankpawagi@gmail.com>
2023-12-15 18:53:18 +0530

    Fix p2

--- evaluator/p2.py
+++ evaluator/p2.py
@@ -23,11 +23,11 @@ def sol(s: str) -> int:
 def eval(s: str) -> str:
     """
     Return the AIC that the student's solution misses.
-    """    
+    """
+    AIC = set()
     expected = sol(s)
     actual = first_positive_integer(s)
-    
-    flag = False
+
     s = s.split()
     for num in s:
         if '1' <= num[0] <= '9':
@@ -38,22 +38,26 @@ def eval(s: str) -> str:
                 else:
                     break
             
-            # AIC 2: Number is suffixed by non-digits
-            if not num[-1].isdigit():
-                if expected != actual:
-                    return "suffix"
-            
-            # AIC 3: Number is prefixed by non-digits or '0'
-            if not num[0].isdigit() or num[0] == '0':
-                if expected != actual:
-                    return "prefix"                
-
+            # AIC 2: First positive integer is suffixed by non-digits
+            if any(not c.isdigit() for c in num):
+                if expected != actual: AIC.add("suffix")
+                
             flag = True
             break
     if not flag:
         # AIC 1: No positive integer in string
-        if actual != 0:
-            return "no positive"
+        if actual != 0: AIC.add("no positive")
+            
+    # AIC 3: First positive integer is prefixed by non-digits or '0'
+    for num in s:
+        digit_indices = [i for i in range(len(num)) if num[i] in '123456789']
+        if digit_indices:
+            first_digit_index = min(digit_indices)
+            if first_digit_index > 0:
+                if expected != actual: AIC.add("prefix")
+            break
+        
+    return AIC       
         
 score = set()
 
@@ -61,9 +65,7 @@ score = set()
 @settings(max_examples=1000)
 def test(s: str):
     global score
-    AIC = eval(s)
-    if AIC:
-        score.add(AIC)
+    score.update(eval(s))
 
 test()
 print(len(score))
