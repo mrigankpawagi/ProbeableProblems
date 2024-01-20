6ee4a7cbb730ba5266695125bbe6bc9748586ade
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-01-20 12:02:31 +0530

    doctest eval runner

--- run_doct.py
+++ run_doct.py
@@ -1,38 +1,62 @@
 import os
 import csv
+import importlib
 
-NUM_QUESTIONS = 5
+# NUM_QUESTIONS = 5
 
 results = []
 
-for Q in range(1, NUM_QUESTIONS + 1):
-    
-    for status in ["buggy", "ok"]:
-        # check if code/qQ/status exists
-        path = f"code/q{Q}/{status}"
-        if not os.path.exists(path):
-            continue
+with open("code/cleaned.csv") as f:
+    data = list(csv.reader(f))[1:]
 
-        # iterate through all the files in code/qQ/status
-        for filename in os.listdir(path):
-            if filename.endswith(".py"):
-                file = os.path.join(path, filename)
-                team, attempt = filename.split("_")
+for team in data:
+    team_number = team[0]
+    tests = team[2:7]
+
+    question_index = 1
+    for test in tests:
+        doctests = []
+        lines = test.split("\n")
+        
+        start_flag = False
+        for line in lines:
+            if line.startswith(">>>"):
+                if start_flag:
+                    doctests.append((temp_args, None))
                 
-                # copy the file into evaluator/ as "submission.py"
-                os.system(f"cp {file} evaluator/submission.py")
-                
-                # run the evaluator and capture the output from stdout
-                output = eval(os.popen(f"python evaluator/p{Q}.py").read().strip())
-                
-                # write to results
-                results.append((Q, team, attempt, output, status))
-            
-# delete the file from evaluator/
-os.system("rm evaluator/submission.py")
+                start_flag = True
+                i, j = line.find("("), line.rfind(")")
+                temp_args = eval(line[i:j+1])
+                if not isinstance(temp_args, tuple):
+                    temp_args = (temp_args,)
+            else:
+                if start_flag:
+                    s = line.strip()
+                    
+                    ### Some special cases
+                    # inf
+                    if 'inf' in s and not ('float("inf")' in s or "float('inf')" in s):
+                        s = s.replace("inf", "float('inf')")
+                    # nan
+                    if 'nan' in s and not ('float("nan")' in s or "float('nan')" in s):
+                        s = s.replace("nan", "float('nan')")
+                    ### End of special cases
+                    
+                    res = eval(s)
+                    doctests.append((temp_args, res))
+                    start_flag = False
+        if start_flag:
+            doctests.append((temp_args, None))
+        
+        # import "test" from doctests_evaluator/p{question_index}.py
+        test_func = importlib.import_module(f"doctests_evaluator.p{question_index}").test
+
+        results.append((question_index, team_number, test_func(doctests)))
+
+        question_index += 1
 
 # save results to csv
-with open("results.csv", "w") as f:
+with open("results_doct.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["Q", "team", "attempt", "score", "status"])
+    writer.writerow(["Q", "team", "score"])
     writer.writerows(results)
